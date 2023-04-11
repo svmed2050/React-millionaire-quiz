f9f887fb0466f2afd643a98081608632b8d7036d
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-11 17:16:23 +0300

    add styles and data for pyramid numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
+import { useState } from 'react'
 import './app.css'
 
 function App() {
+	const [questionNumber, setQuestionNumber] = useState(1)
+
+	const moneyPyramid = [
+		{ id: 1, amount: '$ 100' },
+		{ id: 2, amount: '$ 200' },
+		{ id: 3, amount: '$ 300' },
+		{ id: 4, amount: '$ 500' },
+		{ id: 5, amount: '$ 1000' },
+		{ id: 6, amount: '$ 2000' },
+		{ id: 7, amount: '$ 4000' },
+		{ id: 8, amount: '$ 8000' },
+		{ id: 9, amount: '$ 16000' },
+		{ id: 10, amount: '$ 32000' },
+		{ id: 11, amount: '$ 64000' },
+		{ id: 12, amount: '$ 125000' },
+		{ id: 13, amount: '$ 250000' },
+		{ id: 14, amount: '$ 500000' },
+		{ id: 15, amount: '$ 1000000' },
+	].reverse()
 	return (
 		<div className='app'>
 			<div className='main'>main</div>
 			<div className='pyramid'>
 				<ul className='moneyList'>
-					<li className='moneyListItem active'>
-						<span className='moneyListItemNumber'>4</span>
-						<span className='moneyListItemAmount'>$ 400</span>
-					</li>
-					<li className='moneyListItem'>
-						<span className='moneyListItemNumber'>3</span>
-						<span className='moneyListItemAmount'>$ 300</span>
-					</li>
-					<li className='moneyListItem'>
-						<span className='moneyListItemNumber'>2</span>
-						<span className='moneyListItemAmount'>$ 200</span>
-					</li>
-					<li className='moneyListItem'>
-						<span className='moneyListItemNumber'>1</span>
-						<span className='moneyListItemAmount'>$ 100</span>
-					</li>
+					{moneyPyramid.map((m) => (
+						<li className='moneyListItem'>
+							<span className='moneyListItemNumber'>{m.id}</span>
+							<span className='moneyListItemAmount'>{m.amount}</span>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
