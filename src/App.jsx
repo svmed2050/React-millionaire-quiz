c61baa657300a42a37f0a9cd105f7e6880531903
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-11 19:18:52 +0300

    create and test a timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './app.css'
 import Trivia from './components/Trivia'
+import Timer from './components/Timer'
 
 function App() {
 	const [questionNumber, setQuestionNumber] = useState(1)
@@ -76,23 +77,31 @@ function App() {
 		},
 	]
 
-	const moneyPyramid = [
-		{ id: 1, amount: '$ 100' },
-		{ id: 2, amount: '$ 200' },
-		{ id: 3, amount: '$ 300' },
-		{ id: 4, amount: '$ 500' },
-		{ id: 5, amount: '$ 1000' },
-		{ id: 6, amount: '$ 2000' },
-		{ id: 7, amount: '$ 4000' },
-		{ id: 8, amount: '$ 8000' },
-		{ id: 9, amount: '$ 16000' },
-		{ id: 10, amount: '$ 32000' },
-		{ id: 11, amount: '$ 64000' },
-		{ id: 12, amount: '$ 125000' },
-		{ id: 13, amount: '$ 250000' },
-		{ id: 14, amount: '$ 500000' },
-		{ id: 15, amount: '$ 1000000' },
-	].reverse()
+	const moneyPyramid = useMemo(() => {
+		return [
+			{ id: 1, amount: '$ 100' },
+			{ id: 2, amount: '$ 200' },
+			{ id: 3, amount: '$ 300' },
+			{ id: 4, amount: '$ 500' },
+			{ id: 5, amount: '$ 1000' },
+			{ id: 6, amount: '$ 2000' },
+			{ id: 7, amount: '$ 4000' },
+			{ id: 8, amount: '$ 8000' },
+			{ id: 9, amount: '$ 16000' },
+			{ id: 10, amount: '$ 32000' },
+			{ id: 11, amount: '$ 64000' },
+			{ id: 12, amount: '$ 125000' },
+			{ id: 13, amount: '$ 250000' },
+			{ id: 14, amount: '$ 500000' },
+			{ id: 15, amount: '$ 1000000' },
+		].reverse()
+	}, [])
+
+	useEffect(() => {
+		questionNumber > 1 &&
+			setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount)
+	}, [questionNumber, moneyPyramid])
+
 	return (
 		<div className='app'>
 			<div className='main'>
@@ -101,7 +110,9 @@ function App() {
 				) : (
 					<>
 						<div className='top'>
-							<div className='timer'>30</div>
+							<div className='timer'>
+								<Timer setStop={setStop} questionNumber={questionNumber} />
+							</div>
 						</div>
 						<div className='bottom'>
 							<Trivia
